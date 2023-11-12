7584dc189e17fa8d306e0a8b921cf3bd53635fed
Niksharpkings <67552318+Niksharpkings@users.noreply.github.com>
2023-11-12 08:30:28 -0800

    Version 2.0.5

--- assets/js/game.js
+++ assets/js/game.js
@@ -21,7 +21,7 @@ let fightOrSkip = function () {
     const confirmSkip = window.confirm("Are you sure you'd like to quit?");
     // if yes (true), leave fight
     if (confirmSkip) {
-      window.alert(`${playerInfo.name} has decided to skip this fight. Goodbye!`);
+      window.alert(`${playerInfo.playerName} has decided to skip this fight. Goodbye!`);
       // subtract money from playerMoney for skipping, but don't let them go into the negative
       playerInfo.money = Math.max(0, playerInfo.money - 10);
       return true;
@@ -44,28 +44,28 @@ const fight = (enemy) => {
     damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
     // remove enemy's health by subtracting the amount set in the playerInfo.attack variable
     enemy.health = Math.max(0, enemy.health - damage);
-    console.log(`%c${playerInfo.name} attacked${enemy.name}. ${enemy.name} now has ${enemy.health} health remaining.`, "color: yellow; font-style: italic; background-color: blue;padding: 2px");
+    console.log(`%c${playerInfo.playerName} attacked${enemy.enemyName}. ${enemy.enemyName} now has ${enemy.health} health remaining.`, "color: yellow; font-style: italic; background-color: blue;padding: 2px");
     // check enemy's health
     if (enemy.health <= 0) {
-      window.alert(`${enemy.name} has died!`);
+      window.alert(`${enemy.enemyName} has died!`);
       // award player money for winning
       playerInfo.money += 20;
       // leave while() loop since enemy is dead
       break;
     } else {
-      window.alert(`${enemy.name} still has ${enemy.health} health left.`);
+      window.alert(`${enemy.enemyName} still has ${enemy.health} health left.`);
     }
     // remove players's health by subtracting the amount set in the enemy.attack variable
     damage = randomNumber(enemy.attack - 3, enemy.attack);
     playerInfo.health = Math.max(0, playerInfo.health - damage);
-    console.log(`%c${enemy.name} attacked ${playerInfo.name}. ${playerInfo.name} now has ${playerInfo.health} health remaining.`, "color: yellow; font-style: italic; background-color: blue;padding: 2px");
+    console.log(`%c${enemy.enemyName} attacked ${playerInfo.playerName}. ${playerInfo.playerName} now has ${playerInfo.health} health remaining.`, "color: yellow; font-style: italic; background-color: blue;padding: 2px");
     // check player's health
     if (playerInfo.health <= 0) {
-      window.alert(`${playerInfo.name} has died!`);
+      window.alert(`${playerInfo.playerName} has died!`);
       // leave while() loop if player is dead
       break;
     } else {
-      window.alert(`${playerInfo.name} still has ${playerInfo.health} health left.`);
+      window.alert(`${playerInfo.playerName} still has ${playerInfo.health} health left.`);
     }
   }
 }; // end of fight function
@@ -87,7 +87,7 @@ const startGame = function () {
       pickedEnemyObj.health = randomNumber(40, 60);
       // use debugger to pause script from running and check what's going on at that moment in the code
       // debugger;
-      // pass the playerInfo.money variable's value into the fight function, where it will assume the value of the enemy.name parameter
+      // pass the playerInfo.money variable's value into the fight function, where it will assume the value of the enemy.enemyName parameter
       fight(pickedEnemyObj);
       // if we're not at the last enemy in the array
       if (playerInfo.health > 0 && i < enemyInfo.length - 1) {
@@ -127,19 +127,18 @@ const endGame = function () {
 
 const shop = function () {
   // ask player what they'd like to do
-  let shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice.");
+  let shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE.");
+  shopOptionPrompt = parseInt(shopOptionPrompt);
   // use switch to carry out action
+  //debugger;
   switch (shopOptionPrompt) {
-    case "REFILL":
-    case "refill":
+    case 1:
       playerInfo.refillHealth();
       break;
-    case "UPGRADE":
-    case "upgrade":
+    case 2:
       playerInfo.upgradeAttack();
       break;
-    case "LEAVE":
-    case "leave":
+    case 3:
       window.alert("Leaving the store.");
       // do nothing, so function will end
       break;
@@ -153,20 +152,20 @@ const shop = function () {
 
 /* GAME INFORMATION / VARIABLES */
 // function to set name
-var getPlayerName = function () {
+let getPlayerName = function () {
   //to guarantee entering the loop at least once to prompt the player for the player-robot name
-  var name = "";
-  //condition that checks if name is blank or null
-  while (name === "" || name === null) {
-    name = prompt("What is your robot's name?");
+  let playerName = "";
+  //condition that checks if playerName is blank or null
+  while (playerName === "" || playerName === null) {
+    playerName = prompt("What is your robot's name?");
   }
 
-  console.log("Your robot's name is " + name);
-  return name;
+  console.log("Your robot's name is " + playerName);
+  return playerName;
 };
 // player information
 let playerInfo = {
-  name: getPlayerName(),
+  playerName: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
@@ -200,15 +199,15 @@ let playerInfo = {
 // enemy information
 let enemyInfo = [
   {
-    name: 'Roborto',
+    enemyName: 'Roborto',
     attack: randomNumber(10, 14)
   },
   {
-    name: 'Amy Android',
+    enemyName: 'Amy Android',
     attack: randomNumber(10, 14)
   },
   {
-    name: 'Robo Trumble',
+    enemyName: 'Robo Trumble',
     attack: randomNumber(10, 14)
   }
 ];
@@ -216,7 +215,7 @@ let enemyInfo = [
 console.group("First group");
 console.log(enemyInfo);
 console.log(enemyInfo[0]);
-console.log(enemyInfo[0].name);
+console.log(enemyInfo[0].enemyName);
 console.log(enemyInfo[0]['attack']);
 console.groupEnd();
 /* END GAME INFORMATION / VARIABLES */
